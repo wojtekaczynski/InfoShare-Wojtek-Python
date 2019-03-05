9ceb7647e178811c11ce3f756339fd01343c1735
wojtekkaczynski <wojciechka@tlen.pl>
2019-03-05 20:48:19 +0100

    Pliki z dnia trzeciego

---
+++ 3_Dzien.py
@@ -0,0 +1,24 @@
+##############
+
+# if False:
+#     print ("jestem fajny")
+#     print ("drugi print")
+# elif False
+#     print ("Jestesmy 1")
+#
+#############
+
+# imie = "konstantynopoli"
+# nazwisko:"123"
+#
+# if "z" in imie:
+#     pass
+# else:
+#     print ("Nie ma z w imieniu")
+
+# ##############
+#
+# nazwisko = '123'
+# print(nazwisko.isalpha())
+#
+# ##############
